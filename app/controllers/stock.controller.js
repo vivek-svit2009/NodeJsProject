f69073c67a4434aedbbb3bf065c94a53b415890c
VIVEK KUMAR SHARMA <vivek.svit2009@gmail.com>
2020-04-13 19:07:49 +0530

    Some Changes 12/4/2020

--- app/controllers/stock.controller.js
+++ app/controllers/stock.controller.js
@@ -7,25 +7,50 @@ exports.create = (req,res)=>{
             message: "Note engineNo can not be empty"
         });
     }
-    let bii = Bike.findOne({"bikeModel": req.body.vehicleModel})._id;
+    async function fun(req,res){
+        let bii =   Bike.findOne({"bikeModel": req.body.vehicleModel})._id;
+        return bii;
+    }
+  
+    // Bike.findOne({"bikeModel": req.body.vehicleModel}).exec(function(err,data){
+    //     const stock = new Stock({
+    //         vehicleModelid: data._id,
+    //         challanNo: req.body.challanNo,
+    //         engineNo: req.body.engineNo,
+    //         chachisNo: req.body.chachisNo,
+    //         exshowRoomprice: req.body.exshowRoomprice,
+    //         onRoadprice: req.body.onRoadprice,
+    //     });
+    
+    //     // Save Note in the database
+    //     stock.save()
+    //     .then(data => {
+    //         res.send(data);
+    //     }).catch(err => {
+    //         res.status(500).send({
+    //             message: err.message || "Some error occurred while creating the Stock."
+    //         });
+    //     });
+    // })
     const stock = new Stock({
-        vehicleModelid: bii,
-        challanNo: req.body.challanNo,
-        engineNo: req.body.engineNo,
-        chachisNo: req.body.chachisNo,
-        exshowRoomprice: req.body.exshowRoomprice,
-        onRoadprice: req.body.onRoadprice,
-    });
-
-    // Save Note in the database
-    stock.save()
-    .then(data => {
-        res.send(data);
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Some error occurred while creating the Stock."
-        });
-    });
+                vehicleModelid: fun(),
+                challanNo: req.body.challanNo,
+                engineNo: req.body.engineNo,
+                chachisNo: req.body.chachisNo,
+                exshowRoomprice: req.body.exshowRoomprice,
+                onRoadprice: req.body.onRoadprice,
+            });
+        
+            // Save Note in the database
+            stock.save()
+            .then(data => {
+                res.send(data);
+            }).catch(err => {
+                res.status(500).send({
+                    message: err.message || "Some error occurred while creating the Stock."
+                });
+            });
+   
 };
 exports.findAll = (req,res)=>{
     Stock.find()
